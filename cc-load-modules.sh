bc5751616bbee8cea8e702c0e28d13252cf4f477
William Harvey <williamharvey08@gmail.com>
2019-11-25 16:58:20 -0800

    script for loading modules on computecanada

---
+++ cc-load-modules.sh
@@ -0,0 +1,5 @@
+# Run before using Julia
+module load nixpkgs/16.09  gcc/7.3.0  cuda/10.1 julia/1.2.0 openmpi/3.1.2
+
+# Use before trying to install Julia MPI:
+# export JULIA_MPI_LIBRARY=/cvmfs/soft.computecanada.ca/easybuild/software/2017/avx2/CUDA/gcc7.3/cuda10.1/openmpi/3.1.2/lib/libmpi.so
