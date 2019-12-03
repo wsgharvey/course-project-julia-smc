b74b7dd07573aacff31c0d7e03a7991d965d9a82
Will Harvey <wsgh@pop-os.localdomain>
2019-12-03 12:00:17 -0800

    plotting

---
+++ plot_wasserstein.py
@@ -0,0 +1,51 @@
+import os
+from glob import glob
+import pickle
+
+import matplotlib.pyplot as plt
+
+from wasserstein import wasserstein
+
+def get_fname(particles, steps):
+    return f"empirical-posteriors/posterior-{particles}-{steps}.csv"
+
+def get_cache_name(steps):
+    return f"cache/errors_{steps}.p"
+
+stepses = [10, 100, 1000]
+def get_particleses(steps):
+    template = get_fname("*", str(steps))
+    files = glob(template)
+    def get_particles(fname):
+        return int(fname.replace('-', '.').split('.')[-3])
+    return [get_particles(fname) for fname in files]
+
+empirical_distributions = {
+    steps: get_particleses(steps)
+    for steps in stepses
+}
+
+ground_truth = "empirical-posteriors/posterior-1280-1000.csv"
+
+fig, ax = plt.subplots()
+for steps, particleses in empirical_distributions.items():
+
+    if os.path.exists(get_cache_name(steps)):
+        errors, particleses = \
+            pickle.load(open(get_cache_name(steps), 'rb'))
+        print("loaded from cache")
+    else:
+        errors = [wasserstein(ground_truth,
+                              get_fname(particles, steps))
+                  for particles in particleses]
+        # save to cache for later
+        pickle.dump((errors, particleses,),
+                    open(get_cache_name(steps), 'wb'))
+        print("saved to cache")
+
+    plt.plot(particleses, errors, label=steps)
+    print(f"plotted {steps}")
+
+plt.xscale('log')
+plt.legend()
+plt.savefig('posterior-qualities.pdf', bbox_inches='tight')
--- wasserstein.py
+++ wasserstein.py
@@ -5,26 +5,35 @@ import numpy as np
 import ot
 import argparse
 
-# parse command line arguments (file names)
-import argparse
-parser = argparse.ArgumentParser()
-parser.add_argument("dist1")
-parser.add_argument("dist2")
-args = parser.parse_args()
 
-# load arrays-------------------------------------------------------
-def load_csv(path):
-    return np.loadtxt(open(path, "rb"), delimiter=",", skiprows=1)
-empirical1 = load_csv(f'empirical-posteriors/{args.dist1}.csv')
-empirical2 = load_csv(f'empirical-posteriors/{args.dist2}.csv')
+def wasserstein(path1, path2):
+
+    # load arrays-------------------------------------------------
+    def load_csv(path):
+        return np.loadtxt(open(path, "rb"), delimiter=",", skiprows=1)
+    empirical1 = load_csv(path1)
+    empirical2 = load_csv(path2)
+
+    # create cost matrix------------------------------------------
+    def cost(p1, p2):
+        return ((p1-p2)**2).sum()
+    M = np.array([[cost(sample1, sample2)
+                   for sample2 in empirical2]
+                  for sample1 in empirical1])
+
+    # compute Wasserstein distance--------------------------------
+    return ot.emd2([], [], M)
 
 
-# create cost matrix------------------------------------------------
-def cost(p1, p2):
-    return ((p1-p2)**2).sum()
-M = np.array([[cost(sample1, sample2)
-               for sample2 in empirical2]
-              for sample1 in empirical1])
+if __name__ == '__main__':
+    # parse command line arguments (file names)
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("dist1")
+    parser.add_argument("dist2")
+    args = parser.parse_args()
 
-# compute Wasserstein distance--------------------------------------
-print(ot.emd2([], [], M))
+    print(wasserstein(
+        f'empirical-posteriors/{args.dist1}.csv',
+        f'empirical-posteriors/{args.dist2}.csv'
+    ))
